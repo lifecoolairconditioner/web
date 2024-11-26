f802d38a01367d2cebfc7c0765954a5adbb671c1
azam1660 <azamkaladgi786@gmail.com>
2024-11-27 01:06:23 +0530

    ss

--- src/app/[category]/page.tsx
+++ src/app/[category]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { ChevronLeft, ChevronRight, Filter } from "lucide-react";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 import { getServiceBycategory } from "../../apis/service";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
